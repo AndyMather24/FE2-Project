03842a899ecc045bd5e9fe4014cbe3a7b0e926ff
Andrew Mather <Andy-mather@live.co.uk>
2018-11-05 11:15:01 +0000

    added home component to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
-
 import './App.css';
+import Home from './components/Home/Home';
 
 class App extends Component {
 	render() {
-		return <div className="App" />;
+		return (
+			<div className="App">
+				<Home />
+			</div>
+		);
 	}
 }
 
